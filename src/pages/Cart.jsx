48a5efdb389810d01d1ab4174a60f321a6ffcf7e
Noman Muhammad Nasir <102538287+mnomanmnasir@users.noreply.github.com>
2023-08-16 12:31:57 -0700

    working on routers

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Cart = () => {
+    return (
+
+<>
+<h1>
+    Cart
+</h1>
+</>
+    )
+}
+
+export default Cart;
---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const ProductDetails = () => {
+    return (
+
+<>
+<h1>
+ProductDetails
+</h1>
+</>
+    )
+}
+
+export default ProductDetails;
